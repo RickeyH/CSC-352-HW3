48070b2949d6783ea289910e96730d5e06843668
rickeyhuang <huanr19@wfu.edu>
2021-09-21 02:17:32 -0400

    HW#3 first commit

---
+++ CSCHW3Q5.m
@@ -0,0 +1,6 @@
+% Question 5
+% Input the matrix
+A = [3 -6; 4 -8; 0 1];
+% QR Factorization
+[Q, R] = qr(A);
+
---
+++ CSCHW3Q6.m
@@ -0,0 +1,5 @@
+% Question 6
+% Input the matrix
+A = [4 1 3 2; 8 5 7 6; 12 9 11 10];
+% QR factorization
+[Q, R] = qr(A);
